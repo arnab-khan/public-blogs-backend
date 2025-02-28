c7fad01c0db750aeee5736c7b438cac2976dce85
Arnab Khan <aranabkhan@gmail.com>
2025-03-01 00:00:41 +0530

    token issue fix

--- middlewares/auth-middleware.js
+++ middlewares/auth-middleware.js
@@ -5,9 +5,9 @@ const JWT_SECRET = config.JWT_SECRET;
 const authMiddleware = (req, res, next) => {
     const authHeader = req.headers.authorization;
 
-    if (!authHeader) {
-        return res.status(403).json({ message: "Access denied. No token provided." });
-    }
+    // if (!authHeader) {
+    //     return res.status(403).json({ message: "Access denied. No token provided." });
+    // }
 
     const token = authHeader.split(" ")[1]; // Extract token from "Bearer <token>" format
 
